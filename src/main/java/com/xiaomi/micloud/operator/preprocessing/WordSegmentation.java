0b24845d05e150cd524644dd4d0dd7d1ec97a3a5
johndebugger <johnzyh9669@gmail.com>
2018-09-09 22:38:31 +0800

    Implement wordsegmentation operator

--- src/main/java/com/xiaomi/micloud/operator/preprocessing/WordSegmentation.java
+++ src/main/java/com/xiaomi/micloud/operator/preprocessing/WordSegmentation.java
@@ -3,9 +3,16 @@ package com.xiaomi.micloud.operator.preprocessing;
 import com.rapidminer.operator.Operator;
 import com.rapidminer.operator.OperatorDescription;
 import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.SimpleResultObject;
 import com.rapidminer.operator.ports.InputPort;
 import com.rapidminer.operator.ports.OutputPort;
-//import com.huaban.analysis.jieba.viterbi.JiebaSegmenter;
+
+import com.huaban.analysis.jieba.JiebaSegmenter;
+import com.huaban.analysis.jieba.JiebaSegmenter.SegMode;
+import com.huaban.analysis.jieba.SegToken;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Describe what your operator does.
@@ -20,7 +27,7 @@ public class WordSegmentation extends Operator {
      */
     private InputPort exampleSetInput = getInputPorts().createPort("document");
     private OutputPort exampleSetOutput = getOutputPorts().createPort("document");
-//    private JiebaSegmenter segmenter = new JiebaSegmenter();
+    private JiebaSegmenter segmenter = new JiebaSegmenter();
 
     /**
      * The default constructor needed in exactly this signature
@@ -34,6 +41,10 @@ public class WordSegmentation extends Operator {
 
     @Override
     public void doWork() throws OperatorException {
-        exampleSetOutput.deliver(exampleSetInput.getAnyDataOrNull());
+        String str = exampleSetInput.getData(SimpleResultObject.class).toString();
+        List<SegToken> tokens = segmenter.process(str,SegMode.SEARCH);
+        String result = String.format(Locale.getDefault(), "\n%s\n%s", tokens.toString());
+        SimpleResultObject output = new SimpleResultObject("Document",result);
+        exampleSetOutput.deliver(output);
     }
 }
